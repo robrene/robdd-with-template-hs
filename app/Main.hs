b8bbe94062db8e077d5b3cb4b095f1e559053cd9
Rob Spoel <robspoel@gmail.com>
2016-05-01 22:08:26 +0200

    Create main program to load CNF files for manual testing

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,25 @@
 module Main where
 
+import Data.Array.Unboxed (elems)
+import System.Environment (getArgs)
+import Language.CNF.Parse.ParseDIMACS (parseFile, Clause, clauses)
+
+import BoolExpr.BoolExpr
+import BoolExpr.ROBDD
+
 main :: IO ()
-main = putStrLn "Hello World"
+main = do args <- getArgs
+          cnf <- parseFile $ head args
+          putStrLn . show . build. mkBE $ either (const []) clauses cnf
+          return ()
+
+mkBE :: [Clause] -> BoolExpr
+mkBE cs = foldr (∧) one $ map (mkDisj . elems) cs
+
+mkDisj :: [Int] -> BoolExpr
+mkDisj ids = foldr1 (∨) $ map mkVar ids
+
+mkVar :: Int -> BoolExpr
+mkVar i | i > 0 = var $ i - 1
+mkVar i | i < 0 = neg $ var $ -i - 1
+mkVar _ = undefined
