65b2122369f85bde2faee88728972d99b7afeb00
Rob Spoel <robspoel@gmail.com>
2016-05-09 18:07:38 +0200

    Removed BoolExpr.Compiler for now

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,7 @@
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
+
 import qualified BoolExpr.UnitTests.BoolExpr
-import qualified BoolExpr.UnitTests.Compiler
 import qualified BoolExpr.QuickCheck.BoolExpr
-import qualified BoolExpr.QuickCheck.Compiler
 import qualified BoolExpr.QuickCheck.OBDD
 import qualified BoolExpr.QuickCheck.ROBDD
 import qualified BoolExpr.QuickCheck.ROBDD_TH
@@ -11,11 +11,8 @@ main =
   do putStrLn ""
      putStrLn "BoolExpr unit tests:"
      BoolExpr.UnitTests.BoolExpr.runTests
-     putStrLn "Compiler unit tests:"
-     BoolExpr.UnitTests.Compiler.runTests
      putStrLn ""
      BoolExpr.QuickCheck.BoolExpr.runTests
-     BoolExpr.QuickCheck.Compiler.runTests
      BoolExpr.QuickCheck.OBDD.runTests
      BoolExpr.QuickCheck.ROBDD.runTests
      BoolExpr.QuickCheck.ROBDD_TH.runTests
