790be795044c0eca007c09ca8ad19b7dc942d2ed
Rob Spoel <robspoel@gmail.com>
2016-05-09 18:15:15 +0200

    Simplify BoolExpr by removing pattern synonyms

--- src/BoolExpr/BoolExpr.hs
+++ src/BoolExpr/BoolExpr.hs
@@ -1,13 +1,5 @@
-{-# LANGUAGE PatternSynonyms #-}
-
 module BoolExpr.BoolExpr
-    ( BoolExpr
-    , pattern Var
-    , pattern Zero
-    , pattern One
-    , pattern Not
-    , pattern And
-    , pattern Or
+    ( BoolExpr (..)
     , var
     , zero
     , one
@@ -30,54 +22,41 @@ import BoolExpr.Env
 
 -- Simple and minimal boolean expression definition.
 data BoolExpr =
-    BEVar VarId
-  | BEZero
-  | BEOne
-  | BENot BoolExpr
-  | BEAnd BoolExpr BoolExpr
-  | BEOr BoolExpr BoolExpr
+    Var VarId
+  | Zero
+  | One
+  | Not BoolExpr
+  | And BoolExpr BoolExpr
+  | Or BoolExpr BoolExpr
   deriving (Show, Eq)
-
-pattern Var       :: VarId -> BoolExpr
-pattern Var i     <- BEVar i
-pattern Zero      :: BoolExpr
-pattern Zero      <- BEZero
-pattern One       :: BoolExpr
-pattern One       <- BEOne
-pattern Not       :: BoolExpr -> BoolExpr
-pattern Not e     <- BENot e
-pattern And       :: BoolExpr -> BoolExpr -> BoolExpr
-pattern And e₁ e₂ <- BEAnd e₁ e₂
-pattern Or        :: BoolExpr -> BoolExpr -> BoolExpr
-pattern Or e₁ e₂  <- BEOr e₁ e₂
 
 var :: VarId -> BoolExpr
 var i | i < 0 = error "Can only use natural numbers as variable numbers"
-var i = BEVar i
+var i = Var i
 
 zero :: BoolExpr
-zero = BEZero
+zero = Zero
 
 one :: BoolExpr
-one = BEOne
+one = One
 
 lit :: Bool -> BoolExpr
 lit b = if b then one else zero
 
 neg :: BoolExpr -> BoolExpr
-neg e = BENot e
+neg e = Not e
 
 (∧) :: BoolExpr -> BoolExpr -> BoolExpr
-(∧) e₁ e₂ = BEAnd e₁ e₂
+(∧) e₁ e₂ = And e₁ e₂
 
 (∨) :: BoolExpr -> BoolExpr -> BoolExpr
-(∨) e₁ e₂ = BEOr e₁ e₂
+(∨) e₁ e₂ = Or e₁ e₂
 
 xor :: BoolExpr -> BoolExpr -> BoolExpr
 xor e₁ e₂ = (e₁ ∨ e₂) ∧ (neg $ e₁ ∧ e₂)
 
 (≡) :: BoolExpr -> BoolExpr -> BoolExpr
-(≡) e₁ e₂ = neg $ e₁ `xor` e₂
+(≡) e₁ e₂ = neg (e₁ `xor` e₂)
 
 --
 -- Functions:
@@ -91,7 +70,6 @@ maximumVar (One)       = Nothing
 maximumVar (Not e)     = maximumVar e
 maximumVar (And e₁ e₂) = max (maximumVar e₁) (maximumVar e₂)
 maximumVar (Or e₁ e₂)  = max (maximumVar e₁) (maximumVar e₂)
-maximumVar _ = error "???"
 
 -- Evaluate a boolean expression with an environment. Will cause an error
 -- if the environment is incomplete.
@@ -102,7 +80,6 @@ eval expr env = case expr of Var i     -> env ! i
                              Not e     -> not (eval e env)
                              And e₁ e₂ -> (eval e₁ env) && (eval e₂ env)
                              Or e₁ e₂  -> (eval e₁ env) || (eval e₂ env)
-                             _ -> error "???"
 
 -- Apply some basic simplification rules to a boolean expression.
 simplify :: BoolExpr -> BoolExpr
